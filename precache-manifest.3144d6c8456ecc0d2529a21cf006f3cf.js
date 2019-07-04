8357687adf6272482c331d2563fd8484624e8c93
ginnnnnn <aginlo@hotmail.com>
2019-07-05 00:32:39 +0800

    Updates

---
+++ precache-manifest.3144d6c8456ecc0d2529a21cf006f3cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41bcf4c23a0e5f3d33baa426f1222496",
+    "url": "/ContextApi-and-Reacthooks-theNetNinja-notes/index.html"
+  },
+  {
+    "revision": "0b66257357452b349903",
+    "url": "/ContextApi-and-Reacthooks-theNetNinja-notes/static/css/main.80a22673.chunk.css"
+  },
+  {
+    "revision": "73ce076ac1b620eba87d",
+    "url": "/ContextApi-and-Reacthooks-theNetNinja-notes/static/js/2.fc94a014.chunk.js"
+  },
+  {
+    "revision": "0b66257357452b349903",
+    "url": "/ContextApi-and-Reacthooks-theNetNinja-notes/static/js/main.3c0eaee8.chunk.js"
+  },
+  {
+    "revision": "53da193317e2d4b3c726",
+    "url": "/ContextApi-and-Reacthooks-theNetNinja-notes/static/js/runtime~main.52780e13.js"
+  }
+]);
